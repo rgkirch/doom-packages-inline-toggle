83f2db44c99dfa248164847a8cd47af32cf2ad18
me <me@yogadora.local>
2025-06-24 15:46:56 -0400

    .

--- doom-packages-inline-toggle-test.el
+++ doom-packages-inline-toggle-test.el
@@ -15,7 +15,7 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defmacro my-pkg-hover-test--with-buffer (content &rest body)
+(defmacro doom-packages-inline-toggle-test--with-buffer (content &rest body)
   "Create a temp emacs-lisp-mode buffer with CONTENT and run BODY."
   `(with-temp-buffer
      (emacs-lisp-mode)
@@ -33,32 +33,32 @@
   "Test changing `package!` to `unpin!`."
   (let ((initial-content "(package! some-pkg)")
         (expected-content "(unpin! some-pkg)"))
-    (my-pkg-hover-test--with-buffer initial-content
-      (my-pkg-hover--unpin-package (point-min))
+    (doom-packages-inline-toggle-test--with-buffer initial-content
+      (doom-packages-inline-toggle--unpin-package (point-min))
       (should (string= (buffer-string) expected-content)))))
 
 (ert-deftest doom-packages-inline-toggle--pin-package-test ()
   "Test changing `unpin!` to `package!`."
   (let ((initial-content "(unpin! some-pkg)")
         (expected-content "(package! some-pkg)"))
-    (my-pkg-hover-test--with-buffer initial-content
-      (my-pkg-hover--pin-package (point-min))
+    (doom-packages-inline-toggle-test--with-buffer initial-content
+      (doom-packages-inline-toggle--pin-package (point-min))
       (should (string= (buffer-string) expected-content)))))
 
 (ert-deftest doom-packages-inline-toggle--unpin-package-with-whitespace-test ()
   "Test changing `( package! ...)` to `( unpin! ...)`."
   (let ((initial-content "( package! some-pkg :recipe (:host github))")
         (expected-content "(unpin! some-pkg :recipe (:host github))"))
-    (my-pkg-hover-test--with-buffer initial-content
-      (my-pkg-hover--unpin-package (point-min))
+    (doom-packages-inline-toggle-test--with-buffer initial-content
+      (doom-packages-inline-toggle--unpin-package (point-min))
       (should (string= (buffer-string) expected-content)))))
 
 (ert-deftest doom-packages-inline-toggle--pin-package-with-whitespace-test ()
   "Test changing `( unpin! ...)` to `( package! ...)`."
   (let ((initial-content "( unpin! some-pkg :disable t)")
         (expected-content "(package! some-pkg :disable t)"))
-    (my-pkg-hover-test--with-buffer initial-content
-      (my-pkg-hover--pin-package (point-min))
+    (doom-packages-inline-toggle-test--with-buffer initial-content
+      (doom-packages-inline-toggle--pin-package (point-min))
       (should (string= (buffer-string) expected-content)))))
 
 
@@ -70,16 +70,16 @@
 
 (ert-deftest doom-packages-inline-toggle--render-buttons-for-multiple-forms-test ()
   "Test that buttons for multiple forms are ordered correctly."
-  (my-pkg-hover-test--with-buffer "(package! a) (unpin! b :disable t)"
+  (doom-packages-inline-toggle-test--with-buffer "(package! a) (unpin! b :disable t)"
     (doom-packages-inline-toggle 1) (doom-packages-inline-toggle -1)
-    (my-pkg-hover--render-overlays-for-line 1)
+    (doom-packages-inline-toggle--render-overlays-for-line 1)
     ;; We now expect 4 overlays, one for each button.
-    (should (= (length my-pkg-hover--overlays) 4))
+    (should (= (length doom-packages-inline-toggle--overlays) 4))
     ;; The overlays are created from right to left with increasing priority.
     ;; We can check the text of the overlays to confirm order.
-    ;; `my-pkg-hover--overlays` is built with `push`, so it's in reverse order of creation.
+    ;; `doom-packages-inline-toggle--overlays` is built with `push`, so it's in reverse order of creation.
     (let ((button-texts (mapcar (lambda (ov) (substring-no-properties (overlay-get ov 'after-string)))
-                                my-pkg-hover--overlays)))
+                                doom-packages-inline-toggle--overlays)))
       ;; The visual order is "unpin disable pin enable".
       ;; The creation order (and thus reverse of the final list) is "enable pin disable unpin".
       (should (equal button-texts '(" unpin" " disable" " pin" " enable"))))))
@@ -91,16 +91,16 @@
 (ert-deftest doom-packages-inline-toggle--simulated-click-on-multiple-forms-test ()
   "Test clicking a specific button when multiple are present."
   (let ((initial-buffer "(package! a) (package! b)"))
-    (my-pkg-hover-test--with-buffer initial-buffer
+    (doom-packages-inline-toggle-test--with-buffer initial-buffer
       (doom-packages-inline-toggle 1) (doom-packages-inline-toggle -1)
-      (my-pkg-hover--render-overlays-for-line 1)
+      (doom-packages-inline-toggle--render-overlays-for-line 1)
       ;; We expect 4 overlays: unpin/disable for 'a', and unpin/disable for 'b'
-      (should (= (length my-pkg-hover--overlays) 4))
+      (should (= (length doom-packages-inline-toggle--overlays) 4))
       (let* ((disable-b-overlay
               ;; The overlays are created for 'b' first, then 'a'. The disable
               ;; button is the first one created for 'b'. So it's the last
-              ;; element in `my-pkg-hover--overlays`.
-              (car (last my-pkg-hover--overlays)))
+              ;; element in `doom-packages-inline-toggle--overlays`.
+              (car (last doom-packages-inline-toggle--overlays)))
              (after-string (overlay-get disable-b-overlay 'after-string))
              (disable-b-action (ert-get-button-action after-string)))
         (should (string-match-p " disable" after-string))
@@ -114,7 +114,7 @@
   "Test changing a nested `package!` to `unpin!`."
   (let ((initial-content "(when t\n  (package! tree-inspector))")
         (expected-content "(when t\n  (unpin! tree-inspector))"))
-    (my-pkg-hover-test--with-buffer initial-content
+    (doom-packages-inline-toggle-test--with-buffer initial-content
       (doom-packages-inline-toggle 1) ; Activate to set up internals
       (doom-packages-inline-toggle -1) ; Deactivate to stop timer
 
@@ -123,14 +123,14 @@
       (forward-line 1)
 
       ;; Manually trigger the render for that line
-      (my-pkg-hover--render-overlays-for-line (line-number-at-pos))
+      (doom-packages-inline-toggle--render-overlays-for-line (line-number-at-pos))
 
       ;; There should be two overlays: "unpin" and "disable"
-      (should (= (length my-pkg-hover--overlays) 2))
+      (should (= (length doom-packages-inline-toggle--overlays) 2))
 
       ;; Find the 'unpin' action. It has the higher priority, so it's created last
-      ;; and will be the first element in `my-pkg-hover--overlays`.
-      (let* ((unpin-overlay (car my-pkg-hover--overlays))
+      ;; and will be the first element in `doom-packages-inline-toggle--overlays`.
+      (let* ((unpin-overlay (car doom-packages-inline-toggle--overlays))
              (unpin-action (ert-get-button-action (overlay-get unpin-overlay 'after-string))))
         (should (string-match-p " unpin" (overlay-get unpin-overlay 'after-string)))
         (should unpin-action)
--- doom-packages-inline-toggle.el
+++ doom-packages-inline-toggle.el
@@ -18,16 +18,16 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defvar-local my-pkg-hover--overlays '()
+(defvar-local doom-packages-inline-toggle--overlays '()
   "A buffer-local list that tracks all overlays created by the mode.")
 
-(defvar-local my-pkg-hover--timer nil
+(defvar-local doom-packages-inline-toggle--timer nil
   "The buffer-local timer used for hover detection.")
 
-(defvar-local my-pkg-hover--last-line-number -1
+(defvar-local doom-packages-inline-toggle--last-line-number -1
   "The line number last processed. Used to prevent flickering.")
 
-(defvar-local my-pkg-hover--last-pixel-position nil
+(defvar-local doom-packages-inline-toggle--last-pixel-position nil
   "The raw mouse pixel position last processed. Used for performance.")
 
 
@@ -37,17 +37,17 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun my-pkg-hover--get-sexp-at-point (pt)
+(defun doom-packages-inline-toggle--get-sexp-at-point (pt)
   "Safely return the S-expression starting at point PT."
   (save-excursion
     (goto-char pt)
     (ignore-errors (read (current-buffer)))))
 
-(defun my-pkg-hover--is-target-form-p (form)
+(defun doom-packages-inline-toggle--is-target-form-p (form)
   "Return non-nil if FORM is a `package!` or `unpin!` form."
   (and (listp form) (memq (car form) '(package! unpin!))))
 
-(defun my-pkg-hover--collect-candidate-forms (line-start line-end)
+(defun doom-packages-inline-toggle--collect-candidate-forms (line-start line-end)
   "Collect all `package!` or `unpin!` forms on the current line.
 This version correctly handles nested forms."
   (let ((forms '())
@@ -62,13 +62,13 @@ This version correctly handles nested forms."
               ;; Go to the start of the containing list.
               (backward-up-list 1)
               (let* ((form-start (point))
-                     (form (my-pkg-hover--get-sexp-at-point form-start)))
+                     (form (doom-packages-inline-toggle--get-sexp-at-point form-start)))
                 ;; Check if the found keyword is actually the function call.
-                (when (my-pkg-hover--is-target-form-p form)
+                (when (doom-packages-inline-toggle--is-target-form-p form)
                   (push (cons form form-start) forms))))))))
     (nreverse forms)))
 
-(defun my-pkg-hover--analyze-form (form-info)
+(defun doom-packages-inline-toggle--analyze-form (form-info)
   "Analyze a candidate form to get name, type, status, and boundaries.
 Returns a plist on success, otherwise nil."
   (cl-destructuring-bind ((declaration package-name . rest) . form-start) form-info
@@ -86,19 +86,19 @@ Returns a plist on success, otherwise nil."
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun my-pkg-hover--add-disable-flag (form-end)
+(defun doom-packages-inline-toggle--add-disable-flag (form-end)
   "Navigate to the end of a form and insert ' :disable t'."
   (goto-char (1- form-end))
   (insert " :disable t"))
 
-(defun my-pkg-hover--remove-disable-flag (form-start form-end)
+(defun doom-packages-inline-toggle--remove-disable-flag (form-start form-end)
   "Find and remove ':disable t' within a form's bounds."
   (save-excursion
     (goto-char form-start)
     (when (re-search-forward "[[:space:]]*:disable[[:space:]]+t" form-end t)
       (delete-region (match-beginning 0) (match-end 0)))))
 
-(defun my-pkg-hover--replace-form-symbol (form-start new-symbol-name)
+(defun doom-packages-inline-toggle--replace-form-symbol (form-start new-symbol-name)
   "Replace the first symbol in the form at FORM-START with NEW-SYMBOL-NAME."
   (save-excursion
     (goto-char form-start)
@@ -108,13 +108,13 @@ Returns a plist on success, otherwise nil."
       (delete-region p1 (point))
       (insert new-symbol-name))))
 
-(defun my-pkg-hover--pin-package (form-start)
+(defun doom-packages-inline-toggle--pin-package (form-start)
   "Change an `unpin!` form to a `package!` form."
-  (my-pkg-hover--replace-form-symbol form-start "package!"))
+  (doom-packages-inline-toggle--replace-form-symbol form-start "package!"))
 
-(defun my-pkg-hover--unpin-package (form-start)
+(defun doom-packages-inline-toggle--unpin-package (form-start)
   "Change a `package!` form to an `unpin!` form."
-  (my-pkg-hover--replace-form-symbol form-start "unpin!"))
+  (doom-packages-inline-toggle--replace-form-symbol form-start "unpin!"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -122,23 +122,23 @@ Returns a plist on success, otherwise nil."
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun my-pkg-hover--get-disable-button-def (analysis)
+(defun doom-packages-inline-toggle--get-disable-button-def (analysis)
   "Return a (LABEL . ACTION) cons for the disable/enable button."
   (cl-destructuring-bind (&key is-disabled form-start form-end &allow-other-keys) analysis
     (if is-disabled
-        (list "enable" (lambda () (my-pkg-hover--remove-disable-flag form-start form-end)))
-      (list "disable" (lambda () (my-pkg-hover--add-disable-flag form-end))))))
+        (list "enable" (lambda () (doom-packages-inline-toggle--remove-disable-flag form-start form-end)))
+      (list "disable" (lambda () (doom-packages-inline-toggle--add-disable-flag form-end))))))
 
-(defun my-pkg-hover--get-pin-button-def (analysis)
+(defun doom-packages-inline-toggle--get-pin-button-def (analysis)
   "Return a (LABEL . ACTION) cons for the pin/unpin button."
   (cl-destructuring-bind (&key is-pinned form-start &allow-other-keys) analysis
     (if is-pinned
-        (list "unpin" (lambda () (my-pkg-hover--unpin-package form-start)))
-      (list "pin" (lambda () (my-pkg-hover--pin-package form-start))))))
+        (list "unpin" (lambda () (doom-packages-inline-toggle--unpin-package form-start)))
+      (list "pin" (lambda () (doom-packages-inline-toggle--pin-package form-start))))))
 
-(defvar my-pkg-hover--button-generators
-  (list #'my-pkg-hover--get-disable-button-def
-        #'my-pkg-hover--get-pin-button-def)
+(defvar doom-packages-inline-toggle--button-generators
+  (list #'doom-packages-inline-toggle--get-disable-button-def
+        #'doom-packages-inline-toggle--get-pin-button-def)
   "A list of functions that generate button definitions.
 Each function takes an analysis plist and returns a list of
 \(LABEL ACTION-LAMBDA).")
@@ -150,7 +150,7 @@ Each function takes an analysis plist and returns a list of
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(cl-defun my-pkg-hover--custom-buttonize (string callback &key (face 'lsp-lens-face) (mouse-face 'lsp-lens-mouse-face) data help-echo)
+(cl-defun doom-packages-inline-toggle--custom-buttonize (string callback &key (face 'lsp-lens-face) (mouse-face 'lsp-lens-mouse-face) data help-echo)
   "Create a button from STRING with custom faces.
 When clicked, run CALLBACK. `face' is the default text face,
 and `mouse-face' is the face used on mouse hover."
@@ -164,43 +164,43 @@ and `mouse-face' is the face used on mouse hover."
               'help-echo help-echo
               'button-data data))
 
-(defun my-pkg-hover--cleanup ()
+(defun doom-packages-inline-toggle--cleanup ()
   "Clear all overlays and reset the hover state variables."
-  (my-pkg-hover--clear-overlays)
-  (setq my-pkg-hover--last-line-number -1)
-  (setq my-pkg-hover--last-pixel-position nil))
+  (doom-packages-inline-toggle--clear-overlays)
+  (setq doom-packages-inline-toggle--last-line-number -1)
+  (setq doom-packages-inline-toggle--last-pixel-position nil))
 
-(defun my-pkg-hover--clear-overlays ()
+(defun doom-packages-inline-toggle--clear-overlays ()
   "Delete all overlays managed by this mode in the current buffer."
-  (dolist (ov my-pkg-hover--overlays)
+  (dolist (ov doom-packages-inline-toggle--overlays)
     (delete-overlay ov))
-  (setq my-pkg-hover--overlays '()))
+  (setq doom-packages-inline-toggle--overlays '()))
 
-(defun my-pkg-hover--render-overlays-for-line (line-number)
+(defun doom-packages-inline-toggle--render-overlays-for-line (line-number)
   "Create an individual overlay for each button, with proper spacing."
-  (my-pkg-hover--clear-overlays)
+  (doom-packages-inline-toggle--clear-overlays)
   (let* ((spacing-str (propertize " " 'display '(space :width 1)))
          (line-start (save-excursion (goto-char (point-min)) (forward-line (1- line-number)) (point)))
          (line-end (save-excursion (goto-char line-start) (line-end-position)))
-         (candidate-forms (my-pkg-hover--collect-candidate-forms line-start line-end))
+         (candidate-forms (doom-packages-inline-toggle--collect-candidate-forms line-start line-end))
          (priority 0))
     ;; Process forms from right-to-left, assigning increasing priority so that
     ;; the final display appears left-to-right.
     (dolist (form-info (nreverse candidate-forms))
-      (when-let ((analysis (my-pkg-hover--analyze-form form-info)))
+      (when-let ((analysis (doom-packages-inline-toggle--analyze-form form-info)))
         ;; Iterate over the registered generator functions to create the buttons.
-        (dolist (generator my-pkg-hover--button-generators)
+        (dolist (generator doom-packages-inline-toggle--button-generators)
           (when-let ((button-def (funcall generator analysis)))
             (cl-destructuring-bind (label action) button-def
-              (let* ((button-str (my-pkg-hover--custom-buttonize
+              (let* ((button-str (doom-packages-inline-toggle--custom-buttonize
                                   label
                                   `(lambda (_)
                                      (funcall ,action)
-                                     (my-pkg-hover--cleanup))))
+                                     (doom-packages-inline-toggle--cleanup))))
                      (ov (make-overlay line-end line-end)))
                 (overlay-put ov 'after-string (concat spacing-str button-str))
                 (overlay-put ov 'priority (cl-incf priority))
-                (push ov my-pkg-hover--overlays)))))))))
+                (push ov doom-packages-inline-toggle--overlays)))))))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -209,7 +209,7 @@ and `mouse-face' is the face used on mouse hover."
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun my-pkg-hover--get-mouse-line-number (mouse-data)
+(defun doom-packages-inline-toggle--get-mouse-line-number (mouse-data)
   "Return the line number under the mouse from MOUSE-DATA, or nil."
   (when mouse-data
     (cl-destructuring-bind (frame x . y) mouse-data
@@ -219,7 +219,7 @@ and `mouse-face' is the face used on mouse hover."
                (line-num (and point (line-number-at-pos point))))
           line-num)))))
 
-(defun my-pkg-hover--timer-callback ()
+(defun doom-packages-inline-toggle--timer-callback ()
   "The main timer function that orchestrates the hover effect."
   (let ((current-pos (mouse-pixel-position))
         current-line)
@@ -227,19 +227,19 @@ and `mouse-face' is the face used on mouse hover."
      ;; If mode isn't active in the current window, do nothing.
      ((not (buffer-local-value 'doom-packages-inline-toggle (window-buffer (frame-selected-window)))))
      ;; If mouse is completely still, do nothing.
-     ((equal current-pos my-pkg-hover--last-pixel-position))
+     ((equal current-pos doom-packages-inline-toggle--last-pixel-position))
      ;; If mouse is outside a window, clean up the UI.
-     ((not (setq current-line (my-pkg-hover--get-mouse-line-number current-pos)))
-      (when my-pkg-hover--overlays
-        (my-pkg-hover--cleanup)))
+     ((not (setq current-line (doom-packages-inline-toggle--get-mouse-line-number current-pos)))
+      (when doom-packages-inline-toggle--overlays
+        (doom-packages-inline-toggle--cleanup)))
      ;; If mouse is on the same line, just update pixel position and do nothing else.
-     ((= current-line my-pkg-hover--last-line-number)
-      (setq my-pkg-hover--last-pixel-position current-pos))
+     ((= current-line doom-packages-inline-toggle--last-line-number)
+      (setq doom-packages-inline-toggle--last-pixel-position current-pos))
      ;; Otherwise, we're on a new line, so render overlays.
      (t
-      (setq my-pkg-hover--last-pixel-position current-pos)
-      (setq my-pkg-hover--last-line-number current-line)
-      (my-pkg-hover--render-overlays-for-line current-line)))))
+      (setq doom-packages-inline-toggle--last-pixel-position current-pos)
+      (setq doom-packages-inline-toggle--last-line-number current-line)
+      (doom-packages-inline-toggle--render-overlays-for-line current-line)))))
 
 (define-minor-mode doom-packages-inline-toggle
   "On hover, show overlays for `package!` and `unpin!` forms."
@@ -248,12 +248,12 @@ and `mouse-face' is the face used on mouse hover."
   :keymap nil
   (if doom-packages-inline-toggle
       (progn
-        (my-pkg-hover--cleanup) ; Reset on activation
-        (setq my-pkg-hover--timer
-              (run-with-timer 0.2 0.2 #'my-pkg-hover--timer-callback)))
-    (when (timerp my-pkg-hover--timer)
-      (cancel-timer my-pkg-hover--timer))
-    (my-pkg-hover--cleanup))) ; Reset on deactivation
+        (doom-packages-inline-toggle--cleanup) ; Reset on activation
+        (setq doom-packages-inline-toggle--timer
+              (run-with-timer 0.2 0.2 #'doom-packages-inline-toggle--timer-callback)))
+    (when (timerp doom-packages-inline-toggle--timer)
+      (cancel-timer doom-packages-inline-toggle--timer))
+    (doom-packages-inline-toggle--cleanup))) ; Reset on deactivation
 
 (provide 'doom-packages-inline-toggle)
 
